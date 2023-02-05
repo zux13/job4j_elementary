4c4f3c0f8150777636c9be1a61cd05b26fb15583
zux13 <zux.grr@gmail.com>
2023-02-05 16:52:04 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Rustem Galiullin");
+        System.out.println("20.11.1990");
+    }
+
+}
