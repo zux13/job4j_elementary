925887971eb59b98daa9165cc16cae0c83570ab5
zux13 <zux.grr@gmail.com>
2023-02-10 21:27:30 +0500

    6.5.3. Сортировка выборкой. [#156320 #390387]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -5,9 +5,7 @@ public class SortSelected {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexInRange(data, min, i, data.length - 1);
-            int temp = data[index];
-            data[index] = data[i];
-            data[i] = temp;
+            SwitchArray.swap(data, i, index);
         }
         return data;
     }
