59a560c92c8ed9607165152f9ada75e154b1c638
zux13 <zux.grr@gmail.com>
2023-02-10 10:44:55 +0500

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+import java.sql.Array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
