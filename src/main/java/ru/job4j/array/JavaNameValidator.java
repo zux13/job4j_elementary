cfef3de06236f27bbdea66ddd42fd01a41fd204c
zux13 <zux.grr@gmail.com>
2023-02-13 12:44:05 +0500

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+import static java.lang.Character.isDigit;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean valid = !name.isEmpty();
+        for (int i = 0; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if ((i == 0 && !isLowerLatinLetter(code))
+                    || (!isLowerLatinLetter(code) && !isSpecialSymbol(code)
+                    && !isUpperLatinLetter(code) && !isDigit(code))) {
+                valid = false;
+                break;
+            }
+        }
+        return valid;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
