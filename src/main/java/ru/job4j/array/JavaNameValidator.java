73bdc272535279ece05876f7eb509fa23f13eeb7
zux13 <zux.grr@gmail.com>
2023-02-13 13:59:44 +0500

    6.9. String - это массив [#504884 #391044]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -5,12 +5,14 @@ import static java.lang.Character.*;
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
         boolean valid = !name.isEmpty() && isLowerCase(name.codePointAt(0));
-        for (int i = 1; i < name.length(); i++) {
-            int code = name.codePointAt(i);
-            if (!isLowerLatinLetter(code) && !isSpecialSymbol(code)
-                    && !isUpperLatinLetter(code) && !isDigit(code)) {
-                valid = false;
-                break;
+        if (valid) {
+            for (int i = 1; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (!isLowerLatinLetter(code) && !isSpecialSymbol(code)
+                        && !isUpperLatinLetter(code) && !isDigit(code)) {
+                    valid = false;
+                    break;
+                }
             }
         }
         return valid;
